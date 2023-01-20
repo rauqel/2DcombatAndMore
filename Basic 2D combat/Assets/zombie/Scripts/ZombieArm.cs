2aba5b98a709fdf4836c4dcac4b820f5209c568c
rauqel <ralfonso2021@elam.co.uk>
2023-01-20 15:29:53 +0000

    ..

---
+++ Basic 2D combat/Assets/zombie/Scripts/ZombieArm.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieArm : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D hookRB;
+
+    private void Update()
+    {
+        //hookRB.transform.position = gameObject.transform.position;
+    }
+}
---
+++ Basic 2D combat/Assets/zombie/Scripts/ZombieMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObject;
+    [SerializeField] private float zomSpeed;
+    private Rigidbody2D zomRB;
+
+    private float distance;
+
+    [SerializeField] private Rigidbody2D hookRB;
+    [SerializeField] private Rigidbody2D shoulderRB;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //hookRB.transform.position = gameObject.transform.position;
+
+        ZombieToPlayer();
+    }
+
+    private void ZombieToPlayer()
+    {
+        distance = Vector2.Distance(transform.position, playerObject.transform.position);
+        Vector2 direction = playerObject.transform.position - transform.position;
+        direction.Normalize();
+
+        transform.position = Vector2.MoveTowards
+            (this.transform.position, playerObject.transform.position, zomSpeed * Time.deltaTime);
+
+        
+    }
+}
